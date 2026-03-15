ae0032220b8bcc6f9243ecbc6a467917d0193525
Soliman <abdallah.soliman@capgemini.com>
2026-03-15 15:02:03 +0200

    Fixed bugs with delete keymaps

--- lua/plugins/plugin_files/flash.lua
+++ lua/plugins/plugin_files/flash.lua
@@ -1,7 +1,16 @@
 return {
     'folke/flash.nvim',
     event = 'VeryLazy',
-    opts = {},
+    opts = {
+        modes = {
+            char = {
+                enabled = false,
+            },
+            search = {
+                enabled = false,
+            },
+        },
+    },
     keys = {
         { '<leader>f', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end, desc = 'Flash', },
     },
