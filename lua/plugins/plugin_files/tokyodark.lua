6fba71da13d29bbbf48c713d25385e9b25f6ffd0
Abdallah Soliman <abdallahsoliman03@gmail.com>
2026-06-29 13:12:33 +0300

    Colour change

--- lua/plugins/plugin_files/tokyodark.lua
+++ lua/plugins/plugin_files/tokyodark.lua
@@ -6,6 +6,11 @@ return {
                 diff_blue = '#39d5e3', -- brighter blue
             }
         end,
+        custom_highlights = function(highlights, palette)
+            return {
+                Visual = { bg = "#353945" },
+            }
+        end,
         terminal_colors = false,
     },
     -- config = function(_, opts)
