35a1ed2ae503fa5408e8b58127920286377c69dc
Abdallah Soliman <abdallahsoliman03@gmail.com>
2026-09-06 11:48:46 +0300

    Changed telescope stuff in tokyonight/dark and removed pointless file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -44,7 +44,18 @@ local plugins = {
     'dark2026',
 }
 
-require('plugins.lazy')
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
+end
+
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
+
 require('lazy').setup({
     compile_plugins(plugins),
 })
